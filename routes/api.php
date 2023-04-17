e600853ecbecba77972264a658fe463f244a446d
Peter Elmered <peter@elmered.com>
2023-04-17 09:55:16 +0200

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use Domain\Flights\Http\Controllers\FlightsApiController;
+use Domain\Passengers\Http\Controllers\PassengersApiController;
+use Domain\Tickets\Http\Controllers\TicketsApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+$api_version = 'v1';
+
+Route::middleware(['api', 'throttle:30,1'])
+     ->prefix($api_version.'/')
+     ->group(function (): void {
+         Route::apiResource('tickets', TicketsApiController::class)
+              ->except(['destroy']);
+
+
+         Route::patch('tickets/{ticket}/cancel', [TicketsApiController::class, 'cancel'])
+              ->name('tickets.cancel');
+
+
+         Route::apiResource('flights', FlightsApiController::class)
+              ->only(['show', 'index']);
+
+         Route::get('flights/{flight}/available-seats', [FlightsApiController::class, 'availableSeats'])
+              ->name('flights.available-seats');
+
+         Route::apiResource('passengers', PassengersApiController::class)
+              ->except(['destroy']);
+     });
+
